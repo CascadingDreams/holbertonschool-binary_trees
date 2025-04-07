7a86b31bb7da0f32d93c85f6fa9fb0a6c0466ded
Sammy <myallio94@gmail.com>
2025-04-07 04:16:48 +0000

    task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: the value put in the new node
+ *
+ * Return: pointer to the new node or NULL if failed
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+	if (!new_node)
+	{
+		printf("Memory allocation failed\n");
+		exit(99);
+	}
+
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->n = value;
+
+	return (new_node);
+}
